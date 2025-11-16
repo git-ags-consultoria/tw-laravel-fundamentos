6e44d71e01877ecb26b0f5df11521000d13fbd2d
George Silva <george@agsconsultoria.com.br>
2025-11-16 15:41:12 -0400

    Aula Conhecendo o conceito de rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+
+## Rota padrão Laravel
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+*/
+
+/*
+
+## Aula COnhecendo o conceito de rotas
+
+*/
+
+Route::get('/', function () {
+    echo "Estou na home";
+});
+
+Route::get('/sobre', function () {
+    echo "Estou na página sobre";
+});
+
+Route::get('/servicos', function () {
+    echo "Estou na página de serviços";
+});
+
+Route::get('/contato', function () {
+    echo "Estou na página de contato";
 });
